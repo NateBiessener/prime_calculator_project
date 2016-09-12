629bc925b39b64ed6895380e4fcce5a2dcb01237
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-11 20:59:06 -0500

    add comments to script

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,12 @@
 console.log('script sourced');
 
+//the currently displayed number (currentNum) will be 'stale' if an operatorBtn, '=', or '%' was the last button pressed.
+// If the current number is stale, pressing a numberBtn will start a new number ('y'), and operatorBtn presses will not perform a calculation.
+// If the current number is not stale, pressing a numberBtn will concatenate to the current number, and operatorBtn presses will perform a calculation
+
+//flag to determine whether to reset objectToSend values on numberBtn press
 var summed = false;
+
 var postUrl = '';
 var objectToSend = {
   x: 0,
@@ -8,7 +14,8 @@ var objectToSend = {
 };
 
 var calculate = function () {
-  if (!($('#currentNum').attr('aged'))) {
+  //if currentNum is not stale, use currentNum for y
+  if (!($('#currentNum').attr('stale'))) {
     objectToSend.y = $('#currentNum').html();
   }
   $.ajax({
@@ -17,6 +24,7 @@ var calculate = function () {
     data: objectToSend,
     success: function(data){
       $('#currentNum').html(data.answer);
+      //store result of calculation for possible use in next operation
       objectToSend.x = $('#currentNum').html();
     }//end ajax success callback
   });//end ajax call
@@ -25,14 +33,16 @@ var calculate = function () {
 $(document).ready(function(){
   console.log('jq works');
   $('.numberBtn').on('click', function(){
+    //if last button press was '=', start from scratch
     if (summed){
       objectToSend.x = 0;
       objectToSend.y = 0;
       summed = false;
     }
-    if ($('#currentNum').attr('aged')){
+    //see top of file for notes on 'stale'
+    if ($('#currentNum').attr('stale')){
       $('#currentNum').html($(this).html());
-      $('#currentNum').attr('aged', '');
+      $('#currentNum').attr('stale', '');
     }
     else {
       $('#currentNum').html($('#currentNum').html() + $(this).html());
@@ -45,14 +55,16 @@ $(document).ready(function(){
 
   $('.operatorBtn').on('click', function () {
     summed = false;
-    if (objectToSend.x && !($('#currentNum').attr('aged'))){
+    //see top of file for notes on 'stale'
+    if (objectToSend.x && !($('#currentNum').attr('stale'))){
       calculate();
-      $('#currentNum').attr('aged', 'true');
+      $('#currentNum').attr('stale', 'true');
     }
     else {
       objectToSend.x = $('#currentNum').html();
-      $('#currentNum').attr('aged', 'true');
+      $('#currentNum').attr('stale', 'true');
     }
+    //set postUrl to request the appropriate operation
     switch ($(this).attr('id')) {
       case 'add':
         postUrl = '/add'
@@ -72,41 +84,48 @@ $(document).ready(function(){
   });//end operator click
 
   $('#decimal').on('click', function () {
-    if ($('#currentNum').attr('aged')){
+    //start at 0. if currently displayed number is stale. See top of file for notes on 'stale'.
+    if ($('#currentNum').attr('stale')){
       $('#currentNum').html('0');
     };
+    //will not add '.' if already present in the currentNum
+    //indexOf returns -1 if the provided character is not found in the calling string.
     if ($('#currentNum').html().indexOf('.') === -1){
       $('#currentNum').html($('#currentNum').html() + '.');
-      $('#currentNum').attr('aged', '');
+      $('#currentNum').attr('stale', '');
     }
-  });
+  });//end decimal onclick
 
   $('#signChange').on('click', function () {
+    //0 can't be negative
     if ($('#currentNum').html() === '0'){
       //do nothing
     }
+    //remove '-' sign if present
     else if ($('#currentNum').html()[0] === '-'){
       $('#currentNum').html($('#currentNum').html().slice(1));
     }
+    //else add '-' sign
     else {
       $('#currentNum').html('-' + $('#currentNum').html());
     }
-  });
+  });//end signChange onclick
 
   $('#percent').on('click', function(){
     $('#currentNum').html(Number($('#currentNum').html())/100);
-    $('#currentNum').attr('aged', 'true');
-  });
+    //see top of file for notes on 'stale'
+    $('#currentNum').attr('stale', 'true');
+  });//end percent onclick
 
   $('#calculate').on('click', function(){
     calculate();
-    //store y's value in x for use if '=' is pressed again
-    if (!($('#currentNum').attr('aged'))) {
+    //store y's value in x for use if '=' is the next button pressed. the calculate function will use currentNum for y
+    if (!($('#currentNum').attr('stale'))) {
       objectToSend.x = objectToSend.y;
-      $('#currentNum').attr('aged', 'true');
+      $('#currentNum').attr('stale', 'true');
     }
     summed = true;
-  });
+  });//end calculate onclick
 
   $('#clear').on('click', function () {
     objectToSend.x = 0;
